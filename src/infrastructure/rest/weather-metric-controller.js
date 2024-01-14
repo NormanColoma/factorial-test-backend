845b0aac649f62f599565da63cfb95dd48899b5e
Norman <norman.coloma@voicemode.net>
2024-01-14 19:44:07 +0100

    Adding test for post endpoint

--- src/infrastructure/rest/weather-metric-controller.js
+++ src/infrastructure/rest/weather-metric-controller.js
@@ -2,21 +2,24 @@ const {CREATED} = require('./http-status-code');
 const createWeatherMetricCommandBuilder = require(
     '../../application/create-weather-metric/create-weather-metric-command');
 const container = require('../../container');
+const {isRequestValid} = require('./middleware/rest-validator');
+const {body} = require('express-validator');
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 
 
-router.post('/', async (req, res, next) => {
-  try {
-    const {name, value, timestamp} = req.body;
+router.post('/', [body('name').isString(), body('value').notEmpty(), body('timestamp').isISO8601().toDate()],
+    isRequestValid, async (req, res, next) => {
+      try {
+        const {name, value, timestamp} = req.body;
 
-    const createWeatherMetric = container.resolve('createWeatherMetric');
-    const command = createWeatherMetricCommandBuilder({name, value, timestamp});
-    const response = await createWeatherMetric.create(command);
-    res.status(CREATED).json(response);
-  } catch (err) {
-    next(err);
-  }
-});
+        const createWeatherMetric = container.resolve('createWeatherMetric');
+        const command = createWeatherMetricCommandBuilder({name, value, timestamp});
+        const response = await createWeatherMetric.create(command);
+        res.status(CREATED).json(response);
+      } catch (err) {
+        next(err);
+      }
+    });
 
 module.exports = router;
---
+++ src/test/unit/infrastructure/rest/weather-metric-controller.test.js
@@ -0,0 +1,132 @@
+const supertest = require('supertest');
+const {server, app} = require('../../../..');
+const request = supertest(app);
+const container = require('../../../../container');
+const awilix = require('awilix');
+const {BAD_REQUEST, CREATED, SERVER_ERROR} = require('../../../../infrastructure/rest/http-status-code');
+
+describe('Weather Metric Controller', () => {
+  afterEach(async () => {
+    await server.close();
+    jest.clearAllMocks();
+  });
+
+  describe('POST Create Weather Metric', () => {
+    const createWeatherMetric = {
+      create: jest.fn(),
+    };
+    const idGeneratorMock = {
+      generate: jest.fn(),
+    };
+
+    container.register({
+      createWeatherMetric: awilix.asValue(createWeatherMetric),
+      idGenerator: awilix.asValue(idGeneratorMock),
+    });
+
+    test('should return 400 when name param is missing', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      const res = await request.post('/api/v1/weather-metrics').send();
+      const {status, body, headers} = res;
+      const expectedError = {
+        error: {
+          message: 'Field \'name\' cannot be blank',
+          code: 'fakeCode',
+        },
+      };
+
+      expect(status).toBe(BAD_REQUEST);
+      expect(body).toEqual(expectedError);
+      expect(headers['content-type']).toContain('application/json');
+    });
+
+    test('should return 400 when value param is missing', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      const res = await request.post('/api/v1/weather-metrics')
+          .send({name: 'fakeName'});
+      const {status, body, headers} = res;
+      const expectedError = {
+        error: {
+          message: 'Field \'value\' cannot be blank',
+          code: 'fakeCode',
+        },
+      };
+
+      expect(status).toBe(BAD_REQUEST);
+      expect(body).toEqual(expectedError);
+      expect(headers['content-type']).toContain('application/json');
+    });
+
+    test('should return 400 when timestamp param is missing', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      const res = await request.post('/api/v1/weather-metrics')
+          .send({name: 'fakeName', value: 1});
+      const {status, body, headers} = res;
+      const expectedError = {
+        error: {
+          message: 'Field \'timestamp\' cannot be blank',
+          code: 'fakeCode',
+        },
+      };
+
+      expect(status).toBe(BAD_REQUEST);
+      expect(body).toEqual(expectedError);
+      expect(headers['content-type']).toContain('application/json');
+    });
+
+    test('should return 400 when name param is not a string', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      const res = await request.post('/api/v1/weather-metrics')
+          .send({name: 1, value: 1, timestamp: new Date()});
+      const {status, body, headers} = res;
+      const expectedError = {
+        error: {
+          message: 'Provided value \'name\' has no correct format for field',
+          code: 'fakeCode',
+        },
+      };
+
+      expect(status).toBe(BAD_REQUEST);
+      expect(body).toEqual(expectedError);
+      expect(headers['content-type']).toContain('application/json');
+    });
+
+    test('should return 400 when timestamp param is not a date', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      const res = await request.post('/api/v1/weather-metrics')
+          .send({name: 'fakeName', value: 1, timestamp: 'not valid date'});
+      const {status, body, headers} = res;
+      const expectedError = {
+        error: {
+          message: 'Provided value \'timestamp\' has no correct format for field',
+          code: 'fakeCode',
+        },
+      };
+
+      expect(status).toBe(BAD_REQUEST);
+      expect(body).toEqual(expectedError);
+      expect(headers['content-type']).toContain('application/json');
+    });
+
+    test('should return 201 when weather metric is created', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      const res = await request.post('/api/v1/weather-metrics')
+          .send({name: 'fakeName', value: 1, timestamp: new Date()});
+      const {status, headers} = res;
+
+      expect(status).toBe(CREATED);
+      expect(headers['content-type']).toContain('application/json');
+    });
+
+    test('should return 500 when error is thrown', async () => {
+      idGeneratorMock.generate.mockReturnValue('fakeCode');
+      createWeatherMetric.create.mockRejectedValue(new Error('fakeError'));
+      const res = await request.post('/api/v1/weather-metrics')
+          .send({name: 'fakeName', value: 1, timestamp: new Date()});
+      const {status, headers} = res;
+
+      expect(status).toBe(SERVER_ERROR);
+      expect(headers['content-type']).toContain('application/json');
+    });
+  });
+});
