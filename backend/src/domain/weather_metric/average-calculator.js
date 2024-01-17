68b7ee44e956a403278a56ed17d55085edc56bf4
Norman <norman.coloma@voicemode.net>
2024-01-17 12:59:29 +0100

    Fixing func name in average calculator domain service

--- backend/src/domain/weather_metric/average-calculator.js
+++ backend/src/domain/weather_metric/average-calculator.js
@@ -3,14 +3,14 @@ const WeatherAverage = require('../weather_average/weather-average');
 
 class AverageCalculator {
   calculate(metrics) {
-    const temperatureAvg= this.#calculateTemperatureAvgForMetricName(metrics, weatherMetricTypes.TEMPERATURE);
-    const windSpeedAvg = this.#calculateTemperatureAvgForMetricName(metrics, weatherMetricTypes.WIND_SPEED);
-    const precipitationAvg = this.#calculateTemperatureAvgForMetricName(metrics, weatherMetricTypes.PRECIPITATION);
+    const temperatureAvg= this.#calculateAvgForMetricName(metrics, weatherMetricTypes.TEMPERATURE);
+    const windSpeedAvg = this.#calculateAvgForMetricName(metrics, weatherMetricTypes.WIND_SPEED);
+    const precipitationAvg = this.#calculateAvgForMetricName(metrics, weatherMetricTypes.PRECIPITATION);
     return WeatherAverage.build({temperature: temperatureAvg, windSpeed: windSpeedAvg,
       precipitation: precipitationAvg});
   }
 
-  #calculateTemperatureAvgForMetricName(metrics, metricName) {
+  #calculateAvgForMetricName(metrics, metricName) {
     const temperatureMetrics = metrics.filter((metric) => metric.name === metricName);
     return temperatureMetrics.length > 0 ?
         Math.round(temperatureMetrics.reduce((acc, metric) => acc + metric.value, 0) / temperatureMetrics.length) : 0;
